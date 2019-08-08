0c0662aa15fecde79bb9f19226e386c2da25bbec
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2019-08-07 19:21:01 -0700

    Adapt uart_open() implementation
    Initialize GPIOA and USART1 clocks, configures USART1 Tx and Rx pins,
    and configures and enables USART1.
    
    As of now, I'm unclear of the purpose of the flags parameter as it's not
    used in the code examples

--- HelloWorld/uart.c
+++ HelloWorld/uart.c
@@ -4,8 +4,46 @@
 #include <stm32f10x_usart.h>
 #include "uart.h"
 
+/* @brief Intializes and configures USART1 perifpherals according to specified
+ *			parameters
+ */
 int uart_open(USART_TypeDef *USARTx, uint32_t baud, uint32_t flags)
 {
+	GPIO_InitTypeDef GPIO_InitStructure_Tx;
+	GPIO_InitTypeDef GPIO_InitStructure_Rx;
+	USART_InitTypeDef USART_InitStructure;
+
+	assert_param(IS_USART_123_PERIPH(USARTx));
+	assert_param(IS_USART_BAUDRATE(baud));
+
+	if (USARTx == USART1) {
+		// enable RCC for components required for utilizing USART1
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 |
+								RCC_APB2Periph_AFIO |
+								RCC_APB2Periph_GPIOA, ENABLE);
+
+		// Configure USART1_Tx pin
+		GPIO_StructInit(&GPIO_InitStructure_Tx);
+		GPIO_InitStructure_Tx.GPIO_Pin = GPIO_Pin_9;
+		GPIO_InitStructure_Tx.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_InitStructure_Tx.GPIO_Mode = GPIO_Mode_AF_PP;
+		GPIO_Init(GPIOA, &GPIO_InitStructure_Tx);
+
+		// Configure USART1_Rx pin
+		GPIO_StructInit(&GPIO_InitStructure_Rx);
+		GPIO_InitStructure_Rx.GPIO_Pin = GPIO_Pin_10;
+		GPIO_InitStructure_Rx.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+		GPIO_Init(GPIOA, &GPIO_InitStructure_Rx);
+
+		// Configure USART
+		USART_StructInit(&USART_InitStructure);
+		USART_InitStructure.USART_BaudRate = baud;
+		USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+		USART_Init(USART1, &USART_InitStructure);
+		USART_Cmd(USART1, ENABLE);
+	}
+
+	return 0;
 }
 
 int uart_close(USART_TypeDef *USARTx)
