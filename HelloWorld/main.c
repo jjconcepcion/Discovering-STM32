d010107886c8ccdf372e1ffca7b078e3bb27a9ae
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2019-08-07 20:06:40 -0700

    Initialize timer tick
    Import timer implementation from Blinking_Lights exercise.

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -2,8 +2,34 @@
 #include <stm32f10x_usart.h>
 #include "uart.h"
 
+void Delay(uint32_t nTime);
+
 int main(void) {
+	// Configure SysTick Timer
+	if (SysTick_Config(SystemCoreClock / 1000))
+		while (1);
+
+	while (1) {
+
+		Delay(250);	// wait 250 ms
+	}
+	
 	return 0;
+}
+
+// Timer code
+static __IO uint32_t TimingDelay ;
+
+void Delay(uint32_t nTime)
+{
+	TimingDelay = nTime ;
+	while (TimingDelay != 0);
+}
+
+void SysTick_Handler(void)
+{
+	if (TimingDelay != 0x00)
+	TimingDelay --;
 }
 
 #ifdef USE_FULL_ASSERT
